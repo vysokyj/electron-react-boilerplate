5294d0e3fabf4c37b958a2c35251aa17e8096580
Jiří Vysoký <jiri.vysoky@ys.cz>
2018-03-01 13:47:56 +0100

    Added missing files

---
+++ src/reducers/index.js
@@ -0,0 +1,14 @@
+import { combineReducers } from 'redux';
+import { routerReducer }  from 'react-router-redux';
+import entitiesReducer from './entities';
+import collectionsReducer from './collections';
+import apiReducer from './api';
+
+const rootReducer = combineReducers({
+  api: apiReducer,
+  entities: entitiesReducer,
+  collections: collectionsReducer,
+  router: routerReducer
+});
+
+export default rootReducer;
